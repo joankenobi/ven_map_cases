022d37ddf9dc2f76b6a79e55f702b3d05a3898e5
joankenobi <joan1233blanco@gmail.com>
2024-04-26 19:45:14 -0400

    fix: added autoschema and documentation

--- map/urls.py
+++ map/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
+from rest_framework.documentation import include_docs_urls
 from .views import MapViewSet
 
 router = DefaultRouter()
 router.register('map', MapViewSet)
 urlpatterns = [
     path('', include(router.urls)),
+    path('docs/', include_docs_urls(title='Maps API'))
 ]
 
